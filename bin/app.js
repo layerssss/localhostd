5fa621ac7ff6f66e29ab4108f105484654db1364
LayersSss <layerssss@gmail.com>
2017-06-16 11:39:15 +1200

    Launch app well

--- bin/app.js
+++ bin/app.js
@@ -2,65 +2,58 @@ var electron = require('electron');
 var Path = require('path');
 var Utility = require('../lib/utility.js');
 
-var appListening = false;
-var app, tray;
-var appListen = () => {
-  return Utility.lockPromise('appListen', () =>
-      Promise.resolve()
-      .then(() => {
-        if (appListening) return;
-        return app.listen()
-          .then(() => appListening = true);
-      })
-    )
-    .then(() => tray.setToolTip('Bnb is running...'))
-    .catch(error => {
-      tray.setToolTip(`Bnb is stopped: ${error.message}`);
-      return Promise.reject(error);
-    });
+var app, tray, App;
+
+
+var handleFatelError = error => {
+  console.error(error);
+  electron.dialog.showMessageBox(null, {
+      type: 'error',
+      title: 'Fatel error',
+      message: error.message
+    }, ()=> {
+    process.exit(1);
+  });
 };
 
-if (process.platform == 'darwin') electron.app.dock.hide();
-
 electron.app.on('ready', () => {
-  var App = require('../lib/app.js');
-  app = new App({
-    port: 2999
-  });
+  Promise.resolve()
+    .then(() => {
+      App = require('../lib/app.js');
+      app = new App({
+        port: 2999
+      });
+      tray = new electron.Tray(
+        Path.join(__dirname, '../static/tray.png')
+      );
+    })
+    .then(() => app.listen())
+    .then(() => tray.setToolTip('Bnb is running...'))
+    .then(() => {
+      tray.setContextMenu(electron.Menu.buildFromTemplate([{
+        label: 'Open',
+        click: () => {
+          Promise.resolve()
+            .then(() => app.showWindow())
+            .catch(handleFatelError);
+        },
+      }, {
+        label: 'Quit',
+        click: () => {
+          electron.app.exit();
+        }
+      }]));
+    })
+    .then(()=> {
+      if (process.platform == 'darwin') electron.app.dock.hide();
 
-  tray = new electron.Tray(
-    Path.join(__dirname, '../static/tray.png')
-  );
+      electron.app.on('activate', () => {
+        Promise.resolve()
+          .then(() => app.showWindow())
+          .catch(handleFatelError);
+      });
 
-  tray.setContextMenu(electron.Menu.buildFromTemplate([{
-    label: 'Open',
-    click: () => {
-      appListen()
-        .then(() => app.showWindow())
-        .catch(error => {
-          console.error(error.stack);
-        });
-    },
-  }, {
-    label: 'Quit',
-    click: () => {
-      electron.app.exit();
-    }
-  }]));
-
-
-  appListen()
-    .catch(error => {
-      console.error(error.stack);
-    });
-});
-
-electron.app.on('window-all-closed', () => {});
-
-electron.app.on('activate', () => {
-  appListen()
-    .then(() => app.showWindow())
-    .catch(error => {
-      console.error(error.stack);
-    });
+      electron.app.on('window-all-closed', () => {});
+    })
+    .catch(handleFatelError);
 });
