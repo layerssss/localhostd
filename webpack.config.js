887ef826254651bba6e43e3fd7b7f531c541ee08
Michael Yin <layerssss@gmail.com>
2026-05-27 14:29:49 +1200

    replace CRA with minimal webpack 5 build setup (phase 4a)
    - remove react-scripts, babel-eslint, eslint-config-react-app, eslint-plugin-flowtype
    - add webpack 5, webpack-cli, babel-loader, css-loader, mini-css-extract-plugin, html-webpack-plugin
    - add babel.config.json with preset-env, preset-react, class-properties plugin
    - add webpack.config.js with asset modules for fonts/images
    - replace bin/ui_build.js with direct webpack/webpack-cli calls in package.json scripts
    - drop NODE_OPTIONS=--openssl-legacy-provider (webpack 5 is OpenSSL 3 compatible)

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env = {}) => ({
+  mode: env.development ? "development" : "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "ui_build"),
+    filename: "static/js/[name].[contenthash].js",
+    chunkFilename: "static/js/[name].[contenthash].chunk.js",
+    publicPath: "./",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg|png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: { filename: "static/media/[hash][ext][query]" },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash].css",
+      chunkFilename: "static/css/[name].[contenthash].chunk.css",
+    }),
+  ],
+  devtool: env.development ? "source-map" : false,
+});
